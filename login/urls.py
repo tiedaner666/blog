20693f288d23be3c97dfd5ef004dd1b8c3581a07
tiedaner666 <tiedaner666@gmail.com>
2019-04-22 16:25:37 +0800

    更新login部分功能

---
+++ login/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from login import views
+
+
+urlpatterns = [
+    path('index/', views.index),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('logout/', views.logout),
+
+]
+
